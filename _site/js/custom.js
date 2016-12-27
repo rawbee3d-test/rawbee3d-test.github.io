cb89f28afa5dca41be6abf1878cff22e64a30509
WolfHeadMerchant <wolfheadmerchant@gmail.com>
2016-12-26 19:37:44 -0800

    switch to codesmiths

--- _site/js/custom.js
+++ _site/js/custom.js
@@ -2,3 +2,11 @@
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').focus()
 })
+
+$('#FillableModal').on('shown.bs.modal', function () {
+  $('#FillableInput').focus()
+})
+
+$('#ResetModal').on('shown.bs.modal', function () {
+  $('#ResetInput').focus()
+})
